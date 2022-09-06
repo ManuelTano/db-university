dbe7496e16fdbfc9b63415b114b4867922463ffb
ManuelTano <manueltano98@gmail.com>
2022-09-06 17:23:46 +0200

    QG2

--- queries.sql
+++ queries.sql
@@ -38,8 +38,10 @@
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
+    SELECT COUNT(*) AS `num_insegnanti`, `office_address` FROM `teachers` GROUP BY (`office_address`)
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
+
 
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
