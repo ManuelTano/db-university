15ccf3586630d779c6f22626190e0f863a126593
ManuelTano <manueltano98@gmail.com>
2022-09-07 16:12:59 +0200

    J1 & J2

--- newqueries.sql
+++ newqueries.sql
@@ -1,19 +1,55 @@
 --1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
 
+SELECT `students`.`id`, `students`.`name`, `students`.`surname`, `degrees`.`name`
+FROM `degrees`
+JOIN `students`
+ON `degrees`.`id` = `students`.`degree_id`
+WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 
 --2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 
+SELECT *
+FROM `departments`
+JOIN `degrees`
+ON `departments`.`id` = `degrees`.`department_id` 
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
 
 --3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
+SELECT
+FROM
+JOIN
+ON
+WHERE
 
 --4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 
+SELECT
+FROM
+JOIN
+ON
+WHERE
 
 --5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
+SELECT
+FROM
+JOIN
+ON
+WHERE
 
 --6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 
+SELECT
+FROM
+JOIN
+ON
+WHERE
 
 --7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+
+SELECT
+FROM
+JOIN
+ON
+WHERE
