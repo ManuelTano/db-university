cce1bd9bd39bfbfb28d51cf733087c0cdf5eeb5d
ManuelTano <manueltano98@gmail.com>
2022-09-08 10:16:36 +0200

    Bonus J7 completed

--- newqueries.sql
+++ newqueries.sql
@@ -62,8 +62,13 @@ WHERE `departments`.`name` = 'Dipartimento di Matematica';
 
 --7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
 
-SELECT
-FROM
-JOIN
-ON
-WHERE
+SELECT `students`.`name`, `students`.`surname`, `courses`.`name`, COUNT (`exam_student`.`vote`) AS `tentativi`, MAX(`exam_student`.`vote`) AS `max_vote`
+FROM `students`
+JOIN `exam_student`
+ON `students`.`id` = `exam_student`.`student_id`
+JOIN `exams`
+ON `exams`.`id` = `exam_student`.`exam_id`
+JOIN `courses`
+ON `courses`.`id` = `exams`.`course_id`
+GROUP BY `students`.`id`, `courses`.`id`
+HAVING `max_vote` >= 18;
